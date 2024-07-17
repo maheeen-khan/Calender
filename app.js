5e2360b1c2e14bc114e484fd568e0b59c338f692
maheeen-khan <maheeenkhan4@gmail.com>
2024-07-18 00:39:50 +0500

    updated

--- app.js
+++ app.js
@@ -16,5 +16,18 @@ var myDate = new Date().getDate();
 console.log(myDate );//+ " "+ (myDate.getMonth()+1) + " "+ myDate.getFullYear());
 document.getElementById(myDate).style.backgroundColor = '#01BD93';
 document.getElementById(myDate).style.color = 'white';
-document.getElementById(myDate).style.borderRadius = '10px'
+document.getElementById(myDate).style.borderRadius = '10px';
+
+var month_array = ['JAN', 'FEB', 'MAR', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUG', 'SEP','OCT', 'NOV', 'DEC'];
+var month = month_array[new Date().getMonth()];
+
+var day_array = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+var day = day_array[new Date().getDay()];
+
+var today = day + ", " + myDate + " " + month + " " + new Date().getFullYear();
+
+// document.write(today);
+document.getElementById('end').innerHTML = "<p>" + "TODAY  | " +today + "</p>";
+
 
